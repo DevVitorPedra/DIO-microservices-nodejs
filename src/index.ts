6a270dee5ddad83f31e9d0324ed8a88f3ef0699c
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2021-12-23 11:15:05 -0300

    Config ts/auto-respawn

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('executado')
+import express, {Request,Response,NextFunction} from 'express'
+import dotenv from "dotenv"
+dotenv.config()
+const app = express()
+
+
+app.get('/status',(req:Request,res:Response,next:NextFunction)=>{
+            res.status(200).send({fuz:"ro dar ohhhhhh"})
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("rodando na ",process.env.PORT)
+})
